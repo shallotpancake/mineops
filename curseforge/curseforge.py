c7f6de496bd1eeee42729886744550c6d6367e3d
bean <shallot_pk+github@proton.me>
2025-01-08 20:01:21 -0600

    Extract/install after download

--- curseforge/curseforge.py
+++ curseforge/curseforge.py
@@ -7,6 +7,7 @@ def download_latest_atm10():
     mod_id = 925200 #ATM10 ID
     file_id, date_modified = cf.get_latest_server_pack_id(mod_id)
     print(f"mod_id: {mod_id}\nserver file id: {file_id}\nlast modified: {date_modified}")
-    cf.download_server_pack(mod_id, file_id, date_modified)
+    filename = cf.download_server_pack(mod_id, file_id, date_modified)
+    cf.extract_install_server_pack(filename)
 
 
