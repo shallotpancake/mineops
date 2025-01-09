3a1eac10f4b7d980f196f843402877c5bfdb0438
bean <shallot_pk+github@proton.me>
2025-01-08 20:01:06 -0600

    Add Path constants

--- curseforge/const.py
+++ curseforge/const.py
@@ -1,16 +1,35 @@
 from functools import partial
+from pathlib import Path
 import requests
 
+# Get curseforge API token
 with open('./.curseforge_api_token') as f:
     token = f.read().strip()
 if not token:
-    raise "Failed to find `.curseforge_api_token` in the current directory."
+    raise Exception("Failed to find `.curseforge_api_token` in the current directory.")
 
+# curseforge variables
 API_KEY = token
 BASE_URL = "https://api.curseforge.com/v1"
 MINECRAFT_ID = 432
 ATM10_ID = 925200
 
+# Requests variables
 headers = {"x-api-key": API_KEY}
 
 cfget = partial(requests.get, headers=headers)
+
+# file variables
+SERVER_DIR = Path('server')
+DOWNLOAD_DIR = Path('downloads')
+
+# Make these paths if they don't exist
+Path.mkdir(SERVER_DIR, exist_ok=True)
+Path.mkdir(DOWNLOAD_DIR, exist_ok=True)
+
+# Check if server files exist in SERVER_DIR
+SERVER_INSTALLED = any(SERVER_DIR.iterdir())
+
+# Other
+
+
