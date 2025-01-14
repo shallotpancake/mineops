69208f064cb8cfda99b7eccefb217322f7a73dfd
bean <shallot_pk+github@proton.me>
2025-01-13 22:05:58 -0600

    Use new helper functions

--- curseforge/curseforge.py
+++ curseforge/curseforge.py
@@ -7,9 +7,9 @@ def download_latest_atm10():
 
     print(f"Attempting to download ATM10 server pack...")
     mod_id = 925200 #ATM10 ID
-    file_id, date_modified = cf.get_latest_server_pack_id(mod_id)
-    print(f"mod_id: {mod_id}\nserver file id: {file_id}\nlast modified: {date_modified}")
-    filename = cf.download_server_pack(mod_id, file_id, date_modified)
+    download_url, file_id = cf.get_latest_server_file_url(mod_id)
+    filename = cf.download_server_pack(download_url, file_id)
+    print(f"filename: {filename}")
     cf.extract_server_pack(filename)
 
 
