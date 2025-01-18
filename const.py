ed0a84033a183e7aa53336145658eda9fa0641db
bean <shallot_pk+github@proton.me>
2025-01-18 15:30:41 -0600

    Add log_dir constant

--- const.py
+++ const.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
-# file variables
+# Path variables
 SERVER_DIR = Path('server')
 DOWNLOAD_DIR = Path('downloads')
+LOG_DIR = Path('logs')
+
+# File variables
+TEST_LOG_FILE_PATH = Path('test_output.log')
 
 # secrets
 CURSEFORGE_SECRET_PATH = Path('.curseforge_api_token')
@@ -10,6 +14,7 @@ CURSEFORGE_SECRET_PATH = Path('.curseforge_api_token')
 # Make these paths if they don't exist
 Path.mkdir(SERVER_DIR, exist_ok=True)
 Path.mkdir(DOWNLOAD_DIR, exist_ok=True)
+Path.mkdir(LOG_DIR, exist_ok=True)
 
 # Check if server files exist in SERVER_DIR
 SERVER_INSTALLED = any(SERVER_DIR.iterdir())
